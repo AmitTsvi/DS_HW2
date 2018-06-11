8919b3421171f8fb4b1ad3c78f95d6dab3fcd552
n.l <nitzan.la@campus.technionn.ac.il>
2018-06-12 00:52:19 +0300

    finished DynamicArray, Heap WIP

--- DynamicArray.h
+++ DynamicArray.h
@@ -14,19 +14,39 @@ class DynamicArray {
 
 public:
     DynamicArray(int new_size):  head(nullptr), size(new_size) {
-        head =  new T[new_size];
+        head =  new T[size];
         for (T* ptr=head; ptr < head+size; ptr++) {
             *ptr = nullptr;
         }
     }
 
     ~DynamicArray() {
-
+        delete[] head;
     }
 
     DynamicArray(const DynamicArray& arr) = delete;
 
+    void resize () {
+        T* new_head = new T[size*2];
+        T* new_ptr = new_head;
+        for (T* ptr=head; ptr < head+size; ptr++) {
+            *new_ptr = *ptr;
+            new_ptr++;
+        }
+        for (;new_ptr < (new_head+(size*2)); new_ptr++) {
+            *new_ptr = nullptr;
+        }
+        delete[] head;  //should delete like that?
+        this->head = new_head;
+        this->size = size*2;
+    }
 
+    T& operator[](int index) {
+        if (index >= size) {
+            this->resize();
+        }
+        return *(head+index);
+    }
 };
 
 #endif //WET2_DYNAMICARRAY_H
--- Heap.h
+++ Heap.h
@@ -7,6 +7,7 @@
 
 #include "DynamicArray.h"
 #define nullptr 0
+#define DELETE_INDEX -1
 
 template <class T>
 class Heap {
@@ -14,13 +15,64 @@ class Heap {
     int num_of_elements;
 
 public:
-    Heap(): arr(nullptr){}
-    explicit Heap(DynamicArray<T>& new_arr): arr(&new_arr){}
+    Heap(int size): arr(nullptr), num_of_elements(0) {
+        arr = new DynamicArray<T*>(size);
+    }
+    //explicit Heap(DynamicArray<T>& new_arr): arr(&new_arr), num_of_elements(0){}
     ~Heap() {
 
     }
     Heap (const Heap& heap) = delete;
-    void siftDown ()
+
+    void siftDown (int index) {
+
+    }
+
+    void siftUp(int index) {
+        if (index == 1) {
+            return;
+        }
+        if (arr[index].getId() < arr[index/2].getId()) {
+            T* temp = arr[index];
+            arr[index] = arr[index/2];
+            arr[index/2] = arr->operator[](index);      //why it only let me like this???
+        }
+        return siftUp(index/2);
+    }
+
+    void Insert (T* data) {
+        arr[num_of_elements] = data;
+        siftUp(num_of_elements);
+        num_of_elements++;
+    }
+
+    void decKey (int index, int new_key) {
+        if (arr[index].getId() <= new_key) {
+            return;
+        }
+        int org_key = arr[index].getId();
+        arr[index].setId(new_key);
+        siftUp(index);
+        arr[0].setId(org_key);
+    }
+
+    T* findMin() {
+
+    }
+    void deleteMin() {
+        T* temp = arr[0];
+        arr[0] = arr[num_of_elements-1];
+        arr[num_of_elements-1] = temp;
+        delete arr[num_of_elements-1];      //is this correct??
+        num_of_elements--;
+        siftDown(0);
+    }
+
+    void remove(int index) {
+        this->decKey(index,DELETE_INDEX);
+        this->deleteMin();
+    }
+    //makeHeap should be outside
 };
 
 #endif //WET2_HEAP_H
